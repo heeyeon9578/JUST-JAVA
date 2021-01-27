f10df8428f5f36632cc54c1f74108024e9b66c85
heeyeon <48540554+heeyeon9578@users.noreply.github.com>
2021-01-28 00:14:40 +0900

    Add files via upload

---
+++ Ch3Ex3.java
@@ -0,0 +1,31 @@
+/**
+ * 파일명: Ch3Ex3.java
+ * 작성일: 2021. 1. 28.
+ * 파일설명: 
+ */
+package javabook.ch3;
+
+/**
+ * @author 최희연(heeyeon9578@naver.com)
+ *
+ */
+public class Ch3Ex3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		boolean result = true;
+		float num1 = 32.87f;
+		double num2 = 3.14E5;
+		
+		System.out.printf("result: %s \n", result);
+		System.out.printf("num1: %f \n", num1);
+		System.out.printf("num2: %f \n", num2);
+		
+
+	}
+
+}
