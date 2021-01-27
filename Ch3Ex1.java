351ee465edcef36075209bc09e31df7f2d6afc7b
heeyeon <48540554+heeyeon9578@users.noreply.github.com>
2021-01-27 23:06:45 +0900

    Add files via upload

---
+++ Ch3Ex1.java
@@ -0,0 +1,30 @@
+/**
+ * 파일명: Ch3Ex1.java
+ * 작성일: 2021. 1. 27.
+ * 파일설명: 
+ */
+package javabook.ch3;
+
+/**
+ * @author 최희연(heeyeon9578@naver.com)
+ *
+ */
+public class Ch3Ex1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num1 = 12;
+		int num2 = 20;
+		int result = num1+num2;
+		
+		System.out.println(num1+"+"+ num2);
+		System.out.printf("%d+%d = %d", num1, num2, result);
+		
+
+	}
+
+}
