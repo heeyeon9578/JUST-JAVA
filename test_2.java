dbdb9230a88469bdcbb5b4c18719b64a8c3e6e07
heeyeon <48540554+heeyeon9578@users.noreply.github.com>
2021-02-01 12:19:05 +0900

    Add files via upload

---
+++ test_2.java
@@ -0,0 +1,26 @@
+/**
+ * 파일명: test_2.java
+ * 작성일: 2021. 2. 1.
+ * 파일설명: 문자 k를 알파벳과 아스키 코드로 각각 출력하는 프로그램을 작성
+ */
+package javabook.ch3;
+
+/**
+ * @author 최희연(heeyeon9578@naver.com)
+ *
+ */
+public class test_2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		byte test ='k';
+		System.out.printf("알파벳 k : %c\n",test );
+		System.out.printf("아스키 코드 k : %d", test);
+
+	}
+
+}
