e2417fcecc3039ad3243e92f20bf0deaf657250a
heeyeon <48540554+heeyeon9578@users.noreply.github.com>
2021-02-01 12:16:39 +0900

    Add files via upload

---
+++ Ch3ExX2.java
@@ -0,0 +1,42 @@
+/**
+ * 파일명: Ch3ExX2.java
+ * 작성일: 2021. 1. 28.
+ * 파일설명: if else 문을 사용하여 메뉴처리하기
+ */
+package javabook.ch3;
+import java.util.Scanner;
+
+/**
+ * @author 최희연(heeyeon9578@naver.com)
+ *
+ */
+public class Ch3ExX2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("##메뉴선택##");
+		System.out.println("[1]구매자 [2]판매자");
+		System.out.println("##메뉴를 선택하시오->");
+		
+		String sel =scan.next();
+		
+		if(sel.equals("1")) {
+			System.out.println("환영합니다! 구매자로 로그인하셨습니다.");
+			
+		}
+		else{
+			System.out.println("환영합니다! 판매자로 로그인하셨습니다.");
+			
+		}
+		
+		
+
+	}
+
+}
